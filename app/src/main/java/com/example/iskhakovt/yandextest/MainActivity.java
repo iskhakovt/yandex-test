f64097058583aa119559458c1e869c700ded8858
Timur Iskhakov <iskhakovt@gmail.com>
2016-04-19 00:42:06 +0300

    Toolbar added

--- app/src/main/java/com/example/iskhakovt/yandextest/MainActivity.java
+++ app/src/main/java/com/example/iskhakovt/yandextest/MainActivity.java
@@ -6,22 +6,26 @@
 
 package com.example.iskhakovt.yandextest;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         final ListView listView = (ListView) findViewById(R.id.listView);
         new ListInitTask(listView, this).execute("http://download.cdn.yandex.net/mobilization-2016/artists.json");
 
+        assert(listView != null);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
